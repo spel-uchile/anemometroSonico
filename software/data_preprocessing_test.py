cbf7875d85c366fd40b89fc0de26ac1d8cd09f43
Luis Alberto Herrera <herrera.luis.alberto@gmail.com>
2013-11-21 15:01:38 -0300

    Delete unused test. Renamed test file.

--- software/data_preprocessing_test.py
+++ software/data_preprocessing_test.py
@@ -16,71 +16,41 @@
 #
 # Authors: Karel Mundnich <kmundnic@ing.uchile.cl>
 
-import matplotlib.pyplot as plt
-from scipy.io import netcdf
 import data_preprocessing as dpp
+import utilities
+import numpy as np
+import unittest
 
-path = '/Users/karel/Dropbox/Anemometro/AnemometroSonico/samples/20131108'
-file_noecho = '/noecho/_0090.nc'
-file_v_zero = '/v_zero/_0090.nc'
+class TestDataPreprocessing(unittest.TestCase):
+  
+  def setUp(self):
+    # Data paths
+    file_noecho = './test_data/frame_no_echo.nc'
+    file_v_zero = './test_data/frame_v_zero.nc'
+    file_faulty = './test_data/frame_with_faulty_excitation.nc'
+    file_noexci = './test_data/frame_with_no_excitation.nc'
+    
+    # Data loading
+    self.frame_noecho = utilities.load_data_from_file(file_noecho)
+    self.frame_v_zero = utilities.load_data_from_file(file_v_zero)
+    self.frame_faulty = utilities.load_data_from_file(file_faulty)
+    self.frame_noexci = utilities.load_data_from_file(file_noexci)
+    
+  def test_frame_sanity_check(self):
+    self.assertTrue(dpp.frame_sanity_check(self.frame_noecho))
+    self.assertTrue(dpp.frame_sanity_check(self.frame_v_zero))
+    self.assertFalse(dpp.frame_sanity_check(self.frame_faulty))
+    self.assertFalse(dpp.frame_sanity_check(self.frame_noexci))
+    
+  def test_edge_detection(self):
+    self.assertEqual(dpp.edge_detection(self.frame_noecho), 783)
+    self.assertEqual(dpp.edge_detection(self.frame_v_zero), 810)
 
-# Load data
-f = netcdf.netcdf_file(path + file_noecho, 'r')
-measurement_noecho = f.variables['frame']
-f.close()
-
-f = netcdf.netcdf_file(path + file_v_zero, 'r')
-measurement_v_zero = f.variables['frame']
-f.close()
-
-# Plot raw data
-plt.figure()
-plt.plot(measurement_noecho.data)
-plt.plot(measurement_v_zero.data)
-plt.grid(True)
-plt.title("Raw data")
-plt.legend(('No echo', 'v = 0'))
-
-# Retrieve echoes 
-echoes_noecho = dpp.split_frame(measurement_noecho.data, 1)
-echoes_v_zero = dpp.split_frame(measurement_v_zero.data, 1)
-
-plt.figure()
-plt.subplot(211)
-plt.plot(echoes_v_zero[0]['NORTH'])
-plt.plot(echoes_v_zero[0]['SOUTH'])
-plt.grid(True)
-plt.title('Echoes for v = 0')
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.legend(('NORTH','SOUTH'))
-ymin, ymax = plt.ylim()
-
-plt.subplot(212)
-plt.plot(echoes_noecho[0]['NORTH'])
-plt.plot(echoes_noecho[0]['SOUTH'])
-plt.grid(True)
-plt.title('No echo (calibration info)')
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.legend(('NORTH','SOUTH'))
-plt.ylim(ymin, ymax)
-
-plt.figure()
-plt.subplot(211)
-plt.plot(echoes_v_zero[0]['NORTH'] - echoes_noecho[0]['NORTH'])
-plt.grid(True)
-plt.legend(('NORTH'))
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-ymin, ymax = plt.ylim()
-
-plt.subplot(212)
-plt.plot(echoes_v_zero[0]['SOUTH'] - echoes_noecho[0]['SOUTH'])
-plt.grid(True)
-plt.legend(('SOUTH'))
-plt.xlabel('Samples')
-plt.ylabel('Amplitude')
-plt.ylim(ymin, ymax)
-
-
+  def test_split_frame(self):
+    echoes_v_zero = dpp.split_frame(self.frame_v_zero)
+    self.assertTrue(np.max(echoes_v_zero[0]['NORTH']) in np.arange(400, 600))
+    echoes_faulty = dpp.split_frame(self.frame_faulty)
+    self.assertEqual(echoes_faulty, None)
+    
+if __name__ == '__main__':
+  unittest.main()
